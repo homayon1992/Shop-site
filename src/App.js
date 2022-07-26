c85f889f98aec1eddfd0ffec85e0208fc68e247b
homauon <56159005+homayon1992@users.noreply.github.com>
2022-07-26 01:35:21 -0700

    Completing App.js
    The point of this section:
    Installation is "react-router-dom" ,
    Then we will use it:
    "Switch", "Route", Router".
    
    # npm install react-router-dom
    
    Users will not be able to find Switch in react-router-dom. They need to install versions up to 5 or below 5. Try the below one, which will work.
    
    #npm install react-router-dom@5

--- src/App.js
+++ src/App.js
@@ -2,14 +2,29 @@ import React from 'react';
 import './App.css';
 import Header from './Header.js';
 import Home from './Home';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Home />
+      <Router>
+
+        <Switch>
+          
+
+          <Route path="/">
+            <Header />
+            <Home />
+          </Route>
+
+
+        </Switch>
+
+
+      </Router>
+
     </div>
   );
 }
