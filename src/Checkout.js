db5fe38c174a3acee3f0c50d936e7313e41353f7
homauon <56159005+homayon1992@users.noreply.github.com>
2022-07-26 21:45:06 -0700

    Complete checkout.js

--- src/Checkout.js
+++ src/Checkout.js
@@ -3,8 +3,20 @@ import React from "react";
 
 function Checkout() {
     return (
-        <div>
-            <h1>hi</h1>
+        <div className="checkout">
+            <div className="checkout__left">
+                <img src="https://t3.ftcdn.net/jpg/02/11/28/00/360_F_211280049_g8nsjnEXE2383rW14OQ64Rg2WPANojKK.jpg" alt="" className="checkout__ad" />
+            
+                <div>
+                    <h2 className="checkout__title"> your Shopping Bascket</h2>
+                    
+                </div>
+            </div>
+
+            <div className="checkout__right">
+                <h3> your Baskets sum</h3>
+            </div>
+
         </div>
     )
 }
