2e20c510c25d0a0edfe64a6ba175d4b29acbebf2
homauon <56159005+homayon1992@users.noreply.github.com>
2022-07-26 22:47:52 -0700

    Create CSS
    Create css and import in escript ,

--- src/Checkout.js
+++ src/Checkout.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "./Checkout.css";
 import CheckoutProduct from "./CheckoutProduct"
 import Subtotal from "./Subtotal";
 
--- src/Subtotal.js
+++ src/Subtotal.js
@@ -1,4 +1,5 @@
 import React from 'react' ;
+import "./Subtotal.css"
 import CurrencyFormat from 'react-currency-format';
 
 
