3bfdd3719af2019c5737665227db63dd54991ae8
homauon <56159005+homayon1992@users.noreply.github.com>
2022-07-28 10:29:00 -0700

    add initialState
    use StateProvider

--- src/Product.js
+++ src/Product.js
@@ -1,8 +1,23 @@
 import React from "react";
 import "./Product.css"
+import {useStateValue} from "./StateProvider" ;
 
 
 function Product({id, title, image, price, rating}) {
+
+    const [state, dispatch] = useStateValue();
+    const addToBasket = () => {
+      dispatch({
+        type: "ADD_TO_BASKET",
+        item: {
+          id: id,
+          image: image,
+          price: price,
+          rating: rating,
+        },
+      });
+    };
+
     return ( 
         <div className="product">
             <div className="product__info">
@@ -25,7 +40,7 @@ function Product({id, title, image, price, rating}) {
             <img src={image} />
 
 
-            <button>Add to basket</button>
+            <button onClick="addToBasket">Add to basket</button>
         </div>
     )
 }
