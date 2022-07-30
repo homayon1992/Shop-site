e7605e2a6c2c09cdb413b19151070382d590012b
homauon <56159005+homayon1992@users.noreply.github.com>
2022-07-30 00:25:16 -0700

    Complete Subtotal.js
    First: Add getBasketTotal in reducer.js and return item+ & amount+

--- src/reducer.js
+++ src/reducer.js
@@ -5,6 +5,11 @@ export const initialState = {
 };
 
 // selector
+
+export const getBasketTotal = basket =>
+    basket.reduce((amount, item) => item.price + amount, 0);
+
+
 const reducer = (state, action) => {
 
     switch(action.type) {
